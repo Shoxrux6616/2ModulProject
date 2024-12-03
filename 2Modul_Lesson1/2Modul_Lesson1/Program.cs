9b106208daabd7e3af8255a2979d9ee2a7774e83
Shoxrux6616 <shoxruxsoliqulov@gmail.com>
2024-12-03 09:13:41 +0500

    New

---
+++ 2Modul_Lesson1/2Modul_Lesson1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2Modul_Lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
